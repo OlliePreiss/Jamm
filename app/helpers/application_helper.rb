f1bfad111202c42705dd48619bfcaadbe0e69a85
Adam Steele <adamsteele@hotmail.co.uk>
2023-06-08 16:40:11 +0100

    basic profile show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def ability_to_string(ability)
+    case ability
+    when 0
+      "just beginning"
+    when 1
+      "pretty decent"
+    else
+      "musical god"
+    end
+  end
+
+  def commitment_to_string(commitment)
+    case commitment
+    when 0
+      "very casual"
+    when 1
+      "i want to play semi-regularly"
+    else
+      "very serious commitment"
+    end
+  end
 end
