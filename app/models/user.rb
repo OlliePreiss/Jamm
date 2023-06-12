cf7f7f1a1209a9b736b66dcb10ea6da16a61432a
Adam Steele <adamsteele@hotmail.co.uk>
2023-06-12 12:40:18 +0100

    added cloudinary functionality so audio and pictures can be added to profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ApplicationRecord
   has_many :instruments, through: :user_instruments
   has_many :matches
   has_many :messages
+
+  has_one_attached :sampleurl
+  # validates_attachment_content_type :song, :content_type => [ 'application/octet-stream', 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
   validates :about, length: { minimum: 10, maximum: 100 }
+  has_one_attached :photourl
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
