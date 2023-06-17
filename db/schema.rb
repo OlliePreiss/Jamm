a39f0d4abe3a5036a824ec6a880114d52d5f25dc
Ollie Preiss <76745659+OlliePreiss@users.noreply.github.com>
2023-06-17 12:26:55 +0100

    temp

--- db/schema.rb
+++ db/schema.rb
@@ -140,8 +140,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_141243) do
     t.string "about"
     t.string "sampleurl"
     t.string "photourl"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
