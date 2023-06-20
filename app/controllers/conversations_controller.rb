ce6c37b1563a141346fb3475cd20884d1275d904
Joseph Spinks <jms2895@outlook.com>
2023-06-20 19:55:28 +0100

    messages index frontend updates

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,6 +3,7 @@ class ConversationsController < ApplicationController
     matches = Match.matches_for_user(current_user.id)
     @conversations = matches.map(&:conversation)
     # conversation_last_message = Conversation.find(params[:id])
+    @new_message = Message.new
   end
 
   def show
